58762905c99a9e6bab3aa7b3f6f13afc9cc911db
vuphuongquynh <vuquynh141117@gmail.com>
2023-08-16 09:52:44 +0700

    CreateProject

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program 
+{
+    private static void Main(string[] args)
+    {
+        //khai bao bien kieu string va gan gia tri = Nguyen Van A
+        string hoTen = "Vu Phuong Quynh";
+        //khai bao bien kieu in va gan gia tri = 22
+        int tuoi = 22;
+        Console.WriteLine("Sinh vien {0} - {1} tuoi",hoTen,tuoi);
+    }
+}
