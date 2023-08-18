5c80d6af561557592b78bd8e45828e82a5b7b0e0
vuphuongquynh <vuquynh141117@gmail.com>
2023-08-18 11:45:35 +0700

    CautrucrenhanhSwitchCase

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,11 +1,20 @@
-﻿internal class Program 
+﻿public class Program
 {
     private static void Main(string[] args)
     {
-        //khai bao bien kieu string va gan gia tri = Nguyen Van A
-        string hoTen = "Vu Phuong Quynh";
-        //khai bao bien kieu in va gan gia tri = 5
-        const int namLamViec = 5;
-        Console.WriteLine("Nhan vien {0} - {1} nam kinh nghiem",hoTen,namLamViec);
+        int day = 2;
+        switch(day)
+        {
+            case 1 : System.Console.WriteLine("Chu nhat"); break;
+            case 2 : System.Console.WriteLine("Thu 2"); break;
+            case 3 : System.Console.WriteLine("Thu 3"); break;
+            case 4 : System.Console.WriteLine("Thu 4"); break;
+            case 5 : System.Console.WriteLine("Thu 5"); break;
+            case 6 : System.Console.WriteLine("Thu 6"); break;
+            case 7 : System.Console.WriteLine("Thu 7"); break;
+            default :
+            System.Console.Write("Không phải ngày trong tuần");
+            break;
+        }
     }
 }
