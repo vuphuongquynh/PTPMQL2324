0a9a80979339d46f963adb50c04442a2ccf0bb49
vuphuongquynh <vuquynh141117@gmail.com>
2023-08-18 16:39:21 +0700

    VongLapDoWhile

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,13 +2,13 @@
 {
     private static void Main(string[] args)
     {
-         int a = 1;
-        while(a < 10)
+          int a = 1;
+        do
         {
             System.Console.WriteLine("Vong lap thu {0}", a);
             //tăng a lên 1 đơn vị để đảm bảo có lúc a >= 10
             //a >= 10 thì vòng lặp sẽ kết thúc
             a++;
-        }
+        }while(a < 10);
     }
 }
