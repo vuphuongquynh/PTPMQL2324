6e9d8d073280300d4038cc385633a6a26f09773a
vuphuongquynh <vuquynh141117@gmail.com>
2023-08-18 16:30:39 +0700

    VongLapFor

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,24 +1,10 @@
-﻿internal class Program
+﻿public class Program
 {
     private static void Main(string[] args)
     {
-
-
-        int day = 2;
-        switch(day)
         for(int i = 0; i < 5; i++)
         {
-            case 1 : System.Console.WriteLine("Chu nhat"); break;
-            case 2 : System.Console.WriteLine("Thu 2"); break;
-            case 3 : System.Console.WriteLine("Thu 3"); break;
-            case 4 : System.Console.WriteLine("Thu 4"); break;
-            case 5 : System.Console.WriteLine("Thu 5"); break;
-            case 6 : System.Console.WriteLine("Thu 6"); break;
-            case 7 : System.Console.WriteLine("Thu 7"); break;
-            default :
-            System.Console.Write("Không phải ngày trong tuần");
-            break;
-            System.Console.WriteLine("VVong lap thu {0}", i);
+            System.Console.WriteLine("Vong lap thu {0}", i);
         }
     }
 }
