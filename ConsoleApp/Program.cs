44a45da36bad0ec29a08eabfdd584e5d20ced74b
vuphuongquynh <vuquynh141117@gmail.com>
2023-08-22 10:44:35 +0700

    Gan, lay gia tri cua doi tuong va goi phuong thuc

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,11 +1,16 @@
-﻿public class Program
+﻿using ConsoleApp.Models;
+public class Program
 {
     private static void Main(string[] args)
     {
-          for(int i = 0; i < 10; i++ )
-        {
-            if(i==5) continue;
-            System.Console.WriteLine("Vong lap thu {0}", i);
-        }
+        //khoi tao 2 doi tuong thu class Person
+        Person ps1 = new Person();
+        Person ps2 = new Person();
+        //gan gia tri cho thuoc tinh doi tuong ps1
+        ps1.FullName = "Nguyen Van A";
+        ps1.Address = "Ha Noi";
+        ps1.Age = 18;
+        //goi phuong thuc thuc hien thi thong tin
+        ps1.Display();
     }
 }
