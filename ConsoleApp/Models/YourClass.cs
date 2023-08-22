c7ab2e0e3d06f0597b10f22189cc3717e8009448
vuphuongquynh <vuquynh141117@gmail.com>
2023-08-22 09:38:31 +0700

    Class trong C#

---
+++ ConsoleApp/Models/YourClass.cs
@@ -0,0 +1,19 @@
+namespace ConsoleApp.Models
+{
+    public class Person
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public void EnterData()
+        {
+            System.Console.Write("Full name =");
+            FullName = Console.ReadLine();
+            System.Console.Write("Address = ");
+            Address = Console.ReadLine();
+        }
+        public void Display()
+        {
+            System.Console.WriteLine("{0} - {1}", FullName, Address);
+        }
+    }
+}
