a1cbaba2cceeeb25bfbf4aa37bff8dffe8ef3186
vuphuongquynh <vuquynh141117@gmail.com>
2023-08-29 01:49:08 +0700

    Student ke thua Person

--- ConsoleApp/Models/Student.cs
+++ ConsoleApp/Models/Student.cs
@@ -1,27 +1,7 @@
 namespace ConsoleApp.Models
 {
-    public class Student
+     public class Student : Person
     {
-        public string StudentID { get; set; }
-       public string StudentName { get; set; } 
-       public int StudentAge { get; set; }
-        public int GetYearOfBirth (int age)
-       {
-        int yearofBirth = 2023 - age;
-        return yearofBirth;
-       }
-       public void EnterData()
-       {
-        System.Console.Write("Ma sinh vien = ");
-        StudentID = Console.ReadLine();
-        System.Console.Write("Ten sinh vien = ");
-        StudentName = Console.ReadLine();
-        System.Console.Write("Tuoi = ");
-        StudentAge = Convert.ToInt16(Console.ReadLine());
-       }
-        public void Display()
-       {
-        System.Console.WriteLine("Ma sinh vien: {0} - Ten sinh vien: {1} - Tuoi: {2}",StudentID, StudentName, StudentAge);
-       }
+        public string StudentCode {get; set;}
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,10 +4,9 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        Person ps = new Person();
-        //khai bao 2 bien va gan gia tri
-        string str  = "Nguyen Van A";
-        int a  = 22;
-       Console.WriteLine("{0} sinh nam {1}", str, ps.GetYearOfBirth(a));
+        Student std = new Student();
+		std.EnterData();
+		std.StudentCode = "123456789";
+		std.Display();
     }
 }
