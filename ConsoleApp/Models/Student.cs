f4933a3e4c1f7ec0e7dac99b3e8e1ea88e2a632a
vuphuongquynh <vuquynh141117@gmail.com>
2023-08-29 01:57:40 +0700

    Khoa base ke thua phuong thuc

--- ConsoleApp/Models/Student.cs
+++ ConsoleApp/Models/Student.cs
@@ -3,5 +3,20 @@ namespace ConsoleApp.Models
      public class Student : Person
     {
         public string StudentCode {get; set;}
+    public void EnterData()
+    {
+        //ke thua lai o class Person
+        base.EnterData();
+        // nhap thong tin student code
+        System.Console.Write("Student Code = ");
+        StudentCode = Console.ReadLine();
+    }
+    public void Display()
+    {
+        //Ke thua class Person
+        base.Display();
+        //Nhap thong tin Student code
+        System.Console.Write("Ma sinh vien: {0}", StudentCode);
+    }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,10 @@
 ﻿using ConsoleApp.Models;
 public class Program
-
 {
     private static void Main(string[] args)
     {
         Student std = new Student();
 		std.EnterData();
-		std.StudentCode = "123456789";
 		std.Display();
     }
 }
