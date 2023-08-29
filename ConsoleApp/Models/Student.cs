27ca6210d8dcbd29ff1df94b4a9b853f4ee5f196
vuphuongquynh <vuquynh141117@gmail.com>
2023-08-29 10:59:31 +0700

    Xay dung class ke thua tu Fruit

--- ConsoleApp/Models/Student.cs
+++ ConsoleApp/Models/Student.cs
@@ -1,22 +1,22 @@
 namespace ConsoleApp.Models
 {
-     public class Student : Person
+     public class Student : Fruit
     {
-        public string StudentCode {get; set;}
+        public string hsd {get; set;}
     public void EnterData()
     {
         //ke thua lai o class Person
         base.EnterData();
         // nhap thong tin student code
-        System.Console.Write("Student Code = ");
-        StudentCode = Console.ReadLine();
+        System.Console.Write("Han Su Dung = ");
+        hsd = Console.ReadLine();
     }
     public void Display()
     {
         //Ke thua class Person
         base.Display();
         //Nhap thong tin Student code
-        System.Console.Write("Ma sinh vien: {0}", StudentCode);
+        System.Console.Write("Han Su Dung: {0}", hsd);
     }
     }
 }
